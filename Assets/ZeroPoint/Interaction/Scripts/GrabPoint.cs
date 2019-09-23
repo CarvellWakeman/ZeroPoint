be702cff34e2e698841605bbf9e62bb5f1dfdea4
Zach Lerew <lerewz@oregonstate.edu>
2019-09-22 18:50:37 -0700

    initial commit, crowbar working

---
+++ Assets/ZeroPoint/Interaction/Scripts/GrabPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class GrabPoint : ComplexThrowable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start() called for GrabPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
