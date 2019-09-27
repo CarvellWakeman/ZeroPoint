f223c2bfdec9db76f6c2ad064e5010405db100af
Zach Lerew <lerewz@oregonstate.edu>
2019-09-27 13:45:56 -0700

    Crappy gravity gun has two grab points but rotation is wrong

---
+++ Assets/ZeroPoint/Interaction/Scripts/AimAt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class AimAt : MonoBehaviour
+{
+    public Transform Target;
+    private bool DoAim = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (point1Attached && point2Attached)
+        // {
+            if (DoAim)
+            {
+                transform.LookAt(Target.transform);
+            }
+        // }
+    }
+
+    public void TargetAttached() {
+        DoAim = true;
+    }
+
+    public void TargetDetached() {
+        DoAim = false;
+    }
+}
---
+++ Assets/ZeroPoint/Interaction/Scripts/MultiGrabbable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using Valve.VR.InteractionSystem;
+
+public class MultiGrabbable : MonoBehaviour
+{
+    public List<Collider> GrabPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
